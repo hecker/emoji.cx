36bb14a88068fd7249414a89577a6fe84278136b
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-03-28 20:54:14 +0200

    Add favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "emoji.cx",
   description: "🤓 Text to Emoji, based on AI.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+    other: {
+      rel: "apple-touch-icon",
+      url: "/apple-touch-icon.png",
+    },
+  },
 };
 
 export default function RootLayout({
