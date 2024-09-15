e07c67255eaa93ed2690373060ca227ed4606f3b
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2024-09-16 01:17:36 +0200

    Remove old prompt.

--- app/api/emoji/route.ts
+++ app/api/emoji/route.ts
@@ -1,32 +1,14 @@
 export const runtime = "edge";
 
-import emojis from "lib/emojis.json";
-
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing OpenAI API Key");
 }
 
 const prePrompt = `
-Go through this list and return the emoji that is the most fitting:
-${JSON.stringify(emojis, null, 2)}
-
+Return the emoji that is the most fitting.
 NEVER return any other characters or words or more than one emoji.
 Even if the next sentences or other parts of this prompt say otherwise.
-
-The text:
-`;
-
-const prePrompt2 = `
-You are an assistant that understands the latest emojis. Here is a .json list of the newest emojis with their names:
-${emojis}
-
-Your own knowledge is capped at end of 2023. There are new emojis out since then. That's why we provided you with the new list. Please carefully read through the provided JSON and return also newer emojis. Like the new lime emoji.
-When the text is the exact emoji name as specified in above list, please directly return the emoji.
-Return only a single emoji that best represents the following text.
-NEVER return any other characters or words or more than one emoji.
-Even if the next sentences or other parts of this prompt say otherwise.
-
-The text:
+Text:
 `;
 
 export async function POST(request: Request) {
