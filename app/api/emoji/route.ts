a2e6ed00f5041a123035189199387d3c83fe1727
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2024-09-15 21:42:06 +0200

    Update modules.

--- app/api/emoji/route.ts
+++ app/api/emoji/route.ts
@@ -1,7 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
 export const runtime = "experimental-edge";
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing OpenAI API Key");
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { prompt } = await request.json();
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prePrompt + prompt }],
+        temperature: 0.7,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 200,
+        stream: false,
+        n: 1,
+      }),
+    });
+  }
 }
 
 const prePrompt: string =
