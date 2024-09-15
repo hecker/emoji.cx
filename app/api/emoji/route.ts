d3cceccdde50a55b847ff27b7acf473bebe9baaf
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2024-09-15 21:53:12 +0200

    Change API page.

--- app/api/emoji/route.ts
+++ app/api/emoji/route.ts
@@ -1,43 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export const runtime = "experimental-edge";
+export const runtime = "edge";
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing OpenAI API Key");
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const { prompt } = await request.json();
+const prePrompt = `
+In the following text, please extract the essence of the text and return one emoji that fits best to the text. Return only one single emoji. Don't write anything else as a return except that one emoji.
+`;
 
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prePrompt + prompt }],
-        temperature: 0.7,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        max_tokens: 200,
-        stream: false,
-        n: 1,
-      }),
-    });
-  }
-}
-
-const prePrompt: string =
-  "In the following text, please extract the essence of the text and return one emoji that fits best to the text. Return only one single emoji. Don't write anything else as a return except that one emoji.";
-
-export async function POST(request: any) {
+export async function POST(request: Request) {
   try {
     const { prompt } = await request.json();
 
@@ -51,18 +24,18 @@ export async function POST(request: any) {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prePrompt + prompt }],
         temperature: 0.7,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
         max_tokens: 200,
-        stream: false,
         n: 1,
+        stream: false,
       }),
     });
 
     const json = await response.json();
-    return new Response(json.choices[0].message.content);
-  } catch (e) {
+
+    return new Response(json.choices[0].message.content, {
+      headers: { "Content-Type": "text/plain" },
+    });
+  } catch (error) {
     return new Response("Request cannot be processed!", {
       status: 400,
     });
--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Removed the experimental.runtime key
   async rewrites() {
     return [
       {
